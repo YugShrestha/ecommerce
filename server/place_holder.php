6fad01f07abac42656ee51a681938edc186b1e3b
ystha <yug.shrestha1@gmail.com>
2023-08-30 20:38:38 +0545

    changes

--- server/place_holder.php
+++ server/place_holder.php
@@ -11,16 +11,33 @@ if(isset($_POST['place_order'])){
     $order_cost = $_SESSION['total'];
     $order_status = "on_hold";
     $user_id = 1;
-    $order_date = date('Y-m-d H:i:s'); // Use capital 'Y' for the full year
+    $order_date = date('Y-m-d H:i:s'); 
 
-    // Use consistent variable names (e.g., $phone instead of $user_phone)
+    
     $stmt = $conn->prepare("INSERT INTO orders (order_cost, order_status, user_id, user_phone, user_city, user_address, order_date)
                             VALUES (?, ?, ?, ?, ?, ?, ?)");
     $stmt->bind_param("isissss", $order_cost, $order_status, $user_id, $phone, $city, $address, $order_date);
 
     $stmt->execute();
         $order_id = $stmt->insert_id;
-        echo $order_id;
+     
+
+
+
+        foreach($_SESSION['cart']as $key=>$value){
+            $product=$_SESSION['cart'][$key];
+            $product_id=$product['product_id'];
+                $product_name=$product['product_name'];
+                $product_price=$product['product_price'];
+                $product_image=$product['product_image'];
+                $product_quantity=$product['product_quantity'];
+
+                $stmt1=$conn->prepare("INSERT INTO order_items(order_id,product_id,product_name,product_image,product_price,product_quantity,user_id,order_date)
+                value(?,?,?,?,?,?,?,?)");
+                $stmt1->bind_param('iissiiis',$order_id,$product_id,$product_name,$product_image,$product_price,$product_quantity,$user_id,$order_date);
+                $stmt1->execute();
+
+        }
 
 
 
